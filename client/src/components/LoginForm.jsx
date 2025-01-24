0f3448c8511ddf554ff3c1312207a3a5340ea20a
Marianna <mariannaniko1092@gmail.com>
2025-01-23 21:03:31 -0500

    Update LoginForm.js

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -2,10 +2,12 @@
 import { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
-import { loginUser } from '../utils/API';
 import Auth from '../utils/auth';
+import {LOGIN_USER} from '../utils/mutations'
+
 
 const LoginForm = () => {
+  const [LoginUser] = useMutation(LOGIN_USER)
   const [userFormData, setUserFormData] = useState({ email: '', password: '' });
   const [validated] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
@@ -26,15 +28,9 @@ const LoginForm = () => {
     }
 
     try {
-      const response = await loginUser(userFormData);
+      const {data} = await LoginUser({variables: userFormData});
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
+      Auth.login(data.loginUser.token);
     } catch (err) {
       console.error(err);
       setShowAlert(true);

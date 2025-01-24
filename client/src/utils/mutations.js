1cda079a21c0cf702e74c2724ca10ae43206aefc
Marianna <mariannaniko1092@gmail.com>
2025-01-23 19:24:54 -0500

    Update mutations.js with strings from the sandbox

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,45 @@
+import { gql } from "@apollo/client";
+
+export const LOGIN_USER = gql`
+    mutation LoginUser($email: String!, $password: String!) {
+        loginUser(email: $email, password: $password) {
+        token
+        }
+    }
+`;
+
+export const ADD_USER = gql`
+    mutation AddUser($username: String!, $email: String!, $password: String!) {
+        addUser(username: $username, email: $email, password: $password) {
+        token
+    }
+    }
+`;
+export const SAVE_BOOK = gql`
+    mutation SaveBook($book: BookInput!) {
+    saveBook(book: $book) {
+        savedBooks {
+            authors
+            description
+            bookId
+            image
+            link
+            title
+        }
+    }
+}
+`;
+export const REMOVE_BOOK = gql`
+    mutation RemoveBook($bookId: ID!) {
+        removeBook(bookId: $bookId) {
+            savedBooks {
+            authors
+            description
+            bookId
+            image
+            link
+            title
+            }
+        }
+    }
+`;

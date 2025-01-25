a43293f48c46a9f666a241b8d6284e426de03fa8
Marianna <mariannaniko1092@gmail.com>
2025-01-25 12:52:38 -0500

    Update login and signup

--- client/src/components/SignupForm.jsx
+++ client/src/components/SignupForm.jsx
@@ -1,9 +1,8 @@
 import { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
-
+// import from mutations to be able to use graphQL
 import {ADD_USER} from '../utils/mutations'
 import {useMutation} from '@apollo/client'
-import { createUser } from '../utils/API';
 import Auth from '../utils/auth';
 
 const SignupForm = () => {
